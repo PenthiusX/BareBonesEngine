13f406009887c88da3b08571367a2143c3b70d0f
Aditya <aditya.mattoo88@gmail.com>
2019-02-26 12:02:12 +0530

    addition to the sceneentity class

--- _sceneentity.h
+++ _sceneentity.h
@@ -1,10 +1,9 @@
 #ifndef _SCENEENTITY_H
 #define _SCENEENTITY_H
 #include <qquaternion.h>
-#include <qopenglextrafunctions.h>
 #include <vector>
 
-class _SceneEntity : protected QOpenGLExtraFunctions
+class _SceneEntity
 {
 public:
     _SceneEntity();
@@ -15,23 +14,37 @@ public:
 	//
 	void setPosition(QVector3D pos);//sets the position for the object in the Model matrix via the _renderer class instance.
 	QVector3D getPostion();//get the current position of the relvant object instace
-	//
 	void setRotation(QQuaternion rotation);//sets the rotation for the object in the Model matrix via the _renderer class instance.
 	QQuaternion getRotation();//get the Quaternian value of the rottion of the relavant object instance.
-	//
 	void setScale(float scale);//sets the scale for the object in the Model matrix via the _renderer class instance.
 	float getScale();//get the float  value of the rotation of the relavant object instance.
 	//
-	void setModelData(std::vector<float> vertices,std::vector<float> indices);//Implemntation pending
-	void setShader(char* qrcVetexshaderLocation, char* qrcFragmentshaderLocation);
+	void setVertexData(std::vector<float> vertices);
+	std::vector<float> getvertexData();
+	void setIndexData(std::vector<unsigned int> indices);
+	std::vector<unsigned int> getIndexData();
+	void setuvData(std::vector<int> uvCoords);
+	std::vector<int> getUvData();
+	void setnormalData(std::vector<float> normalData);
+	std::vector<float> getNormalData();
+	//
+	void setModelData(std::vector<float> vertices,std::vector<unsigned int> indices);//Implemntation pending
+	void setShader(char* qrcVetexshaderLocation, char* qrcFragmentshaderLocation);//Implementation pending
 
 private:
 	int id;
-	QVector3D postion;
-	QQuaternion rotation;
-	int scale;
+	QVector3D postion;//
+	QQuaternion rotation;//
+	int scale;//
+	std::vector<float>vertexData;//
+	std::vector<unsigned int> indexData;//
+	std::vector<int> uvData;//
+	std::vector<float> normalData;//
 
-	void setModelMatrix();
+	char* vShaderPath;
+	char* fShaderPath;
+
+	void calculateNormals();//Runs after Vertices have been fed , Implementation pending.
 };
 
 #endif // _SCENEENTITY_H
