6511fb936be11be16782d87c575a3086b5bd3fc0
Aditya <aditya.mattoo88@gmail.com>
2019-03-13 17:37:00 +0530

    added Assimp dependecies and Assist importer class

--- _assetloader.h
+++ _assetloader.h
@@ -1,24 +1,25 @@
 #ifndef _ASSETLOADER_H
 #define _ASSETLOADER_H
 #include <vector>
-
+#include <qstring.h>
+//Assimp headers
+#include <assimp/scene.h>;
+#include <assimp/postprocess.h>;
+#include <assimp/Importer.hpp>;
 
 class _AssetLoader
 {
 public:
     _AssetLoader();
     ~_AssetLoader();
-	//
-    void setAssetVertices(std::vector<float> vert);
+
     std::vector<float> getAssetVertices();
-	//
-    void setAssetIndices(std::vector<unsigned int> ind);
     std::vector<unsigned int> getAssetIndices();
+	bool modelLoader(QString pathToFile);
 
 private:
 std::vector<float> vertices;
 std::vector<unsigned int> indices;
-
 };
 
 #endif // _ASSETLOADER_H
---
+++ deps/windows/assimp-4.0.1/test/headercheck/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/prophesys-1050t/Desktop/HiraPheri/DiamondPal/DiamondPal/deps/assimp-4.0.1/test/headercheck
+# Build directory: C:/Users/prophesys-1050t/Desktop/HiraPheri/DiamondPal/DiamondPal/deps/assimp-4.0.1/test/headercheck
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
