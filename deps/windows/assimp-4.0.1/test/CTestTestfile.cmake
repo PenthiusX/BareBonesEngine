510a3527eb0a10245d3afb2a39e26fbcb20a2989
Aditya <aditya.mattoo88@gmail.com>
2019-03-14 11:25:44 +0530

    cleanup

--- deps/windows/assimp-4.0.1/test/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/prophesys-1050t/Desktop/HiraPheri/DiamondPal/DiamondPal/deps/assimp-4.0.1/test
-# Build directory: C:/Users/prophesys-1050t/Desktop/HiraPheri/DiamondPal/DiamondPal/deps/assimp-4.0.1/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unittests "unit")
-set_tests_properties(unittests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/prophesys-1050t/Desktop/HiraPheri/DiamondPal/DiamondPal/deps/assimp-4.0.1/test/CMakeLists.txt;164;add_test;C:/Users/prophesys-1050t/Desktop/HiraPheri/DiamondPal/DiamondPal/deps/assimp-4.0.1/test/CMakeLists.txt;0;")
-subdirs("headercheck")
