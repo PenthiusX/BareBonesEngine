b2c06425e77d8c3df44c7fcbbdb9eb52c60f8cb4
Aditya <aditya.mattoo88@gmail.com>
2019-03-27 12:23:28 +0530

    intigrating of textures in progress

--- _renderer.h
+++ _renderer.h
@@ -32,42 +32,37 @@ public:
     void setShader(QString vertexShader, QString fragmentShader);//takes a string literal and passes
     void setModelDataInBuffers(std::vector<float>vertexArray,std::vector<unsigned int> indexArray);//take vertex and index data and binds it to object buffer
     void setTexture(char* texBitmap);//takes am image and binds it to object
-     void setupTexture();//takes am image and binds it to object
+    void setTexture(QString pathtoTexture);//takes am image and binds it to object
+    void setupTexture();//takes am image and binds it to object
     void setModelMatrix(QVector3D position, float scale, QQuaternion rotation);//set the model matrix
     void setCamViewMatrix(QVector3D eyePos, QVector3D focalPoint, QVector3D upVector);//sets the Camera matrix
     void setProjectionMatrix(int resW, int resH, float fov, float zFar, float zNear);//sets the projection matrix
     void updateTrasformations(QVector3D pos,QQuaternion, float scale);
-	void updateTrasformations(QVector3D pos, QQuaternion rot);
-	void updateTrasformations(QVector3D pos);
-	void setSceneEntityInRenderer(_SceneEntity s);
-	_SceneEntity getSceneEntity();
+    void updateTrasformations(QVector3D pos, QQuaternion rot);
+    void updateTrasformations(QVector3D pos);
+    void setSceneEntityInRenderer(_SceneEntity s);
+    _SceneEntity getSceneEntity();
     void draw();//Draws everything bound in the scene
 
 private:
-unsigned int VBO;//vertex buffer object
-unsigned int VAO;//attribute buffer object
-unsigned int EBO;//index buffer object
-//Shader class object sets the shaders and passes
-//the program to the current context
-_Shader* shdr;
-//Matrices for Translation and view
-//will be multiplied with the position to set translation
-//rotaion ,scaling witrespect to view.
-glm::mat4 glm_model4x4;
-glm::mat4 glm_projection4x4;
-glm::mat4 glm_view4x4;
-//Stores the uniform location allocated in the shader
-int colorUniform,mvpUniform,modelUnifrom,viewUniform,projectionUniform;
-std::vector<_Renderer> sceneObject;
-//Holds the vertex and index data
-std::vector<float> vertices;//not allocated yet
-std::vector<unsigned int> indices;//not allocated yet
-//
-QElapsedTimer timer;
-//
-_SceneEntity sceneEntity;
-//
-std::vector<_Texture> textures;
+    unsigned int VBO;//vertex buffer object
+    unsigned int VAO;//attribute buffer object
+    unsigned int EBO;//index buffer object
+    //Shader class object sets the shaders and passes the program to the current context
+    _Shader* shdr;
+    //Matrices for Translation and view will be multiplied with the position to set translation rotaion ,scaling witrespect to view.
+    glm::mat4 glm_model4x4;
+    glm::mat4 glm_projection4x4;
+    glm::mat4 glm_view4x4;
+    //Stores the uniform location allocated in the shader
+    int colorUniform,mvpUniform,modelUnifrom,viewUniform,projectionUniform;
+    std::vector<_Renderer> sceneObject;
+    //Holds the vertex and index data
+    std::vector<float> vertices;//not allocated yet
+    std::vector<unsigned int> indices;//not allocated yet
+    QElapsedTimer timer;//timer object to maintain a timer based events and trasformations
+    _SceneEntity sceneEntity;//the local sceneEntity object for use in the renderer
+    std::vector<_Texture> textures;//Teture array for tetures in use for the respective renderer object
 
 };
 
--- shaders/vshader.glsl
+++ shaders/vshader.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 uniform vec4 aColor;
+//
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
@@ -10,7 +11,7 @@ out vec4 ourColor;
 
 void main()
 {
-	mat4 mvpx = projection * view * model;
+    mat4 mvpx = projection * view * model;
     gl_Position =  mvpx * vec4(aPos, 1.0);
-	ourColor = aColor;
+    ourColor = aColor;
 }
