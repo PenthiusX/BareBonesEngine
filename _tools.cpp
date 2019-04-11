17800863357a2880706513bb1ddd61dd6da0c872
Aditya <aditya.mattoo88@gmail.com>
2019-04-11 12:22:18 +0530

    push befor commiting to developing the raycast clicking

--- _tools.cpp
+++ _tools.cpp
@@ -9,7 +9,11 @@ _Tools::_Tools()
 {
 
 }
-
+/*
+ * Function:ReadStringFromQrc(QString Filename)
+ * returns the string litral for a qrc file path
+ * Created: 2_03_2019
+*/
 QString _Tools::ReadStringFromQrc(QString Filename)
 {
     // Read text files from .qrc file
@@ -27,7 +31,13 @@ QString _Tools::ReadStringFromQrc(QString Filename)
         mFile.close();
         return mText;
 }
-
+/*
+ * Function : retunrnMaxPoint(QVector2D mousepos)
+ * this funtion takes in the updated mousposition and
+ * returns the max point wrt to the first point passed
+ * into this function.
+ * Created: 11_04_2019
+*/
 QVector2D _Tools::retunrnMaxPoint(QVector2D mousepos)
 {
     QVector2D max;
@@ -39,6 +49,19 @@ QVector2D _Tools::retunrnMaxPoint(QVector2D mousepos)
     {
          max.setY(max.y());
     }
-
     return max;
 }
+/*
+ * Function : translateToPoint(QVector3D pointLocation, float stepDistance)
+ * this function takes in two points and generates the next point to translate
+ * towards the point to focus on.
+ * Created: 11_04_2019
+*/
+QVector3D _Tools::translateToPoint(QVector3D pointLocation, float stepDistance)
+{
+    //implementation pending
+
+    QVector3D points;
+    return points;
+}
+
